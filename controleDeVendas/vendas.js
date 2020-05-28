6b335bd8fb107740c198bc952edaad77ed84dc32
paulofagnerdeoliveira <44214940+paulofagnerdeoliveira@users.noreply.github.com>
2020-05-28 00:41:37 -0300

    202005280041

---
+++ controleDeVendas/vendas.js
@@ -0,0 +1,11 @@
+class venda{
+    constructor(quantidade, precoUnitario, totalVenda,DataDeVenda){
+          this.quantidade = quantidade;
+          this.precoUnitario = precoUnitario;
+          this.totalVenda = totalVenda;
+          this.DataDeVenda = DataDeVenda;
+          
+    }
+    toJson(){return JSON.parse(JSON.stringify(this));}
+    parse(json){let nJson = JSON.parse(JSON.stringify(json));for(let k in nJson){if(k in this){this[k] = nJson[k];}}return this;}
+  }
