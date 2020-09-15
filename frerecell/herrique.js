257b7724a689eee8635132f7315eed93827654b4
paulofagnerdeoliveira <44214940+paulofagnerdeoliveira@users.noreply.github.com>
2020-09-14 23:13:23 -0300

    140920202313

--- frerecell/herrique.js
+++ frerecell/herrique.js
@@ -4,11 +4,11 @@
   const contactMee = document.querySelector("#contatoMee");
 
   contactMee.onclick = function(){
-    window.open ("https://www.instagram.com/henriqueeletronicos1/?hl=pt-br",'_blank');
+    window.open ("https://www.instagram.com/frerecell/?hl=pt-br",'_blank');
   }
   
   contactMe.onclick = function(){
-      window.open("https://api.whatsapp.com/send?phone=553193680945",'_blank');
+      window.open("https://api.whatsapp.com/send?phone=5531993581414",'_blank');
     }
 
   
