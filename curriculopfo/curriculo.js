c5d6d942372a1a1914f58587a8eb667aede735d7
paulofagnerdeoliveira <44214940+paulofagnerdeoliveira@users.noreply.github.com>
2020-11-29 20:34:39 -0300

    203429112020

--- curriculopfo/curriculo.js
+++ curriculopfo/curriculo.js
@@ -1,5 +1,12 @@
 const contactMe = document.querySelector("#contactMe");
+const email = document.querySelector("#email")
 
 contactMe.onclick = ()=>{
     window.open("https://api.whatsapp.com/send?phone=5531993581414",'_blank');
   }
+
+  email.onclick = () =>{
+    window.open("https://mail.google.com/mail/u/0/#inbox","_blank")
+}
+
+ 
