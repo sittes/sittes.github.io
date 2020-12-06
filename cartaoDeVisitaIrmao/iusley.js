163d1160197fa22871b07b4c4decd34dabc19622
paulofagnerdeoliveira <44214940+paulofagnerdeoliveira@users.noreply.github.com>
2020-12-06 12:48:54 -0300

    124806122020

---
+++ cartaoDeVisitaIrmao/iusley.js
@@ -0,0 +1,17 @@
+
+  const contactMe = document.querySelector("#contactMe");
+
+  const contactMee = document.querySelector("#contatoMee");
+
+  contactMee.onclick = function(){
+    window.open ("https://www.instagram.com/frerecell/?hl=pt-br",'_blank');
+  }
+  
+  contactMe.onclick = function(){
+      window.open("https://api.whatsapp.com/send?phone=5531992045012",'_blank');
+    }
+
+  
+  
+
+   
