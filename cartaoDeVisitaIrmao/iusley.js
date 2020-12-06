0cbc3df950d3a44d301396d6d268f50273ea2a78
paulofagnerdeoliveira <44214940+paulofagnerdeoliveira@users.noreply.github.com>
2020-12-06 13:52:55 -0300

    135206122020

--- cartaoDeVisitaIrmao/iusley.js
+++ cartaoDeVisitaIrmao/iusley.js
@@ -4,8 +4,9 @@
   const contactMee = document.querySelector("#contatoMee");
 
   contactMee.onclick = function(){
-    window.open ("https://www.instagram.com/frerecell/?hl=pt-br",'_blank');
+    window.open ("https://www.instagram.com/iusley_dias/?hl=pt-br",'_blank');
   }
+  
   
   contactMe.onclick = function(){
       window.open("https://api.whatsapp.com/send?phone=5531992045012",'_blank');
