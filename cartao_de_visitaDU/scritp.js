a1c1be18c918a3175a85b204c3796bb4800bad05
oasis-technology <44214940+oasis-technology@users.noreply.github.com>
2021-08-25 22:44:29 -0300

    25082021

---
+++ cartao_de_visitaDU/scritp.js
@@ -0,0 +1,18 @@
+
+  const contactMe = document.querySelector("#contactMe");
+
+  const contactMee = document.querySelector("#contatoMee");
+
+  contactMee.onclick = function(){
+    window.open ("https://www.instagram.com/luizeduardoedumendes/?hl=pt-br",'_blank');
+  }
+  
+  
+  contactMe.onclick = function(){
+      window.open("https://api.whatsapp.com/send?phone=5533999211569",'_blank');
+    }
+
+  
+  
+
+   
