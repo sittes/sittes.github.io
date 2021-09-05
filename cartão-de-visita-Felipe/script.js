34df4d6f3ef83e4ec4023e4e7abf233808cbc61b
sittes <44214940+sittes@users.noreply.github.com>
2021-09-05 11:55:38 -0300

    Add files via upload

---
+++ cartão-de-visita-Felipe/script.js
@@ -0,0 +1,18 @@
+
+  const contactMe = document.querySelector("#contactMe");
+
+  const contactMee = document.querySelector("#contatoMee");
+
+  contactMee.onclick = function(){
+    window.open ("https://www.instagram.com/plp_pinturas/?hl=pt-br",'_blank');
+  }
+  
+  
+  contactMe.onclick = function(){
+      window.open("https://api.whatsapp.com/send?phone=55033988563951",'_blank');
+    }
+
+  
+  
+
+   
