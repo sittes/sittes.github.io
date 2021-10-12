81ccd5c4a5fa9745191b76a1b1e276fb7b3a6bc5
paulofagnerdeoliveira <paulo.fagnerdeoliveira@gmail.com>
2021-10-11 22:18:20 -0300

    202110112216

---
+++ ContaBancaria/ContaBancaria.php
@@ -0,0 +1,14 @@
+<?php
+
+class ContaBancaria{
+   public $banco;
+   public $nomeTitular;
+   public $numeroAgencia;
+   public $numeroConta;
+   public $saldo;
+}
+
+new ContaBancaria();
+
+
+var_dump($conta);
