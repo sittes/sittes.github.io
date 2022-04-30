faee31f0d500878492684172bcd4d6deaca390a6
sittes <44214940+sittes@users.noreply.github.com>
2022-04-30 16:29:25 -0300

    Add files via upload

---
+++ Ivip/paulofagner.js
@@ -0,0 +1,17 @@
+
+  const contactMe = document.querySelector("#contactMe");
+
+  const contactMee = document.querySelector("#contatoMee");
+
+  contactMee.onclick = function(){
+    window.open ("https://www.instagram.com/ivipcoin/",'_blank');
+  }
+  
+  contactMe.onclick = function(){
+      window.open("https://t.me/ivipcoin",'_blank');
+    }
+
+  
+  
+
+   
